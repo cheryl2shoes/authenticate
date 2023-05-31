80e1fda18e27251277361547623218b61680fae0
Cheryl_Moshoeshoe <ccmoshoeshoe@gmail.com>
2023-06-01 01:34:25 +0200

    modified:   src/main/java/com/bank/authenticate/AuthenticateController.java

--- src/main/java/com/bank/authenticate/AuthenticateController.java
+++ src/main/java/com/bank/authenticate/AuthenticateController.java
@@ -1,16 +1,26 @@
 package com.bank.authenticate;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
 public class AuthenticateController {
 
 
-    private List<Users> db= List.of(new Users("Cheryl Moshoeshoe","ch!!ER07"));
+    private Map<String,Users> db =new HashMap<>(){{
+        put("Cheryl Moshoeshoe",new Users("Cheryl Moshoeshoe","ch!!ER07"));
+    }};
+    //private List<Users> db= List.of(new Users("Cheryl Moshoeshoe","ch!!ER07"));
 
 
 
@@ -20,9 +30,18 @@ public class AuthenticateController {
     }
 
     @GetMapping("/authenticate")
-    public List<Users> get(){
+    public Collection<Users> get(){
 
-        return db;
+        return db.values();
+    
+    }
+    @GetMapping("/authenticate/{username}")
+    public Users get(@PathVariable String username){
+
+        Users user =db.get(username);
+        if (user == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return user;
     
     }
     
