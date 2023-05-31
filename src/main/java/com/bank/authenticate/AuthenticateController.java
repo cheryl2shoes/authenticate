3cba11dfd320c4b79d0e76dcad060fbab1966217
Cheryl_Moshoeshoe <ccmoshoeshoe@gmail.com>
2023-06-01 00:09:35 +0200

    renamed:    src/main/java/com/AuthenticateController.java -> src/main/java/com/bank/authenticate/AuthenticateController.java 	new file:   src/main/java/com/bank/authenticate/Users.java

--- src/main/java/com/bank/authenticate/AuthenticateController.java
+++ src/main/java/com/bank/authenticate/AuthenticateController.java
@@ -1,4 +1,6 @@
-package com;
+package com.bank.authenticate;
+
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticateController {
 
 
+    private List<Users> db= List.of(new Users("Cheryl Moshoeshoe","ch!!ER07"));
+
+
 
     @GetMapping("/")
     public String Dumela(){
         return "Madume, Rea ya ho amohela ha Standard Bank";
+    }
+
+    @GetMapping("/authenticate")
+    public List<Users> get(){
+
+        return db;
+    
     }
     
 
---
+++ src/main/java/com/bank/authenticate/Users.java
@@ -0,0 +1,35 @@
+package com.bank.authenticate;
+
+public class Users {
+    
+   
+    private String username;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Users() {
+    }
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+   
+
+    
+}
