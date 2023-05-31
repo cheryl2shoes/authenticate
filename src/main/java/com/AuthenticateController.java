20abf5bf9675aac4ef58807fc815a9020285db14
Cheryl_Moshoeshoe <ccmoshoeshoe@gmail.com>
2023-05-31 23:20:22 +0200

    modified:   src/main/java/com/AuthenticateController.java

--- src/main/java/com/AuthenticateController.java
+++ src/main/java/com/AuthenticateController.java
@@ -1,8 +1,18 @@
 package com;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class AuthenticateController {
+
+
+
+    @GetMapping("/")
     public String Dumela(){
         return "Madume, Rea ya ho amohela ha Standard Bank";
     }
     
+
 }
